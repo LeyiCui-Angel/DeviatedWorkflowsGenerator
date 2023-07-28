e7d29faa1e849747a6b04a77f2dc0c4a8536a96b
Angel Cui <69516825+LeyiCui-Angel@users.noreply.github.com>
2023-07-28 01:26:59 -0400

    Add download output function
    - app.py
    - index.html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file, Response
 from werkzeug.utils import secure_filename
 from WorkflowGen import *
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -32,6 +33,19 @@ def home():
             output = permutation(workflow.split(';'), level)
 
     return render_template('index.html', output="\n".join([str(elem) for elem in output]) if output else None, data=data)
+
+@app.route('/download_output', methods=['POST'])
+def download_output():
+    output = request.form.get('output')
+
+    if output is not None:
+        bytes_io = BytesIO()
+        bytes_io.write(output.encode('utf-8'))
+        bytes_io.seek(0)
+
+        return send_file(bytes_io, mimetype="text/plain", as_attachment=True, attachment_filename="output.txt")
+
+    return Response("No output found.", status=404)
 
 if __name__ == '__main__':
     app.run(debug=True)
